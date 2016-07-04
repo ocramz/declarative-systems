ade2ceb43fa114c207b71c3318711e3e788dd89a
ocramz <zocca.marco@gmail.com>
2016-07-04 17:49:16 +0200

    adding design annotations

--- src/System/Declarative/Entities/Environment.hs
+++ src/System/Declarative/Entities/Environment.hs
@@ -1,2 +1,7 @@
 module System.Declarative.Entities.Environment where
 
+
+-- | EXAMPLE QUERIES
+-- observe : does the current environment contain a variable X ?
+-- assert : does an env.var. X (if it exists) have value Y ?
+
--- src/System/Declarative/Entities/File.hs
+++ src/System/Declarative/Entities/File.hs
@@ -1,1 +1,8 @@
 module System.Declarative.Entities.File where
+
+
+
+
+-- | EXAMPLE QUERIES
+-- does filepath X exist ?
+-- does a file X contain a line that begins with Y ? (<- too general)
