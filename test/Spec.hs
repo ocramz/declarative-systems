500b40ce77daada5ba3c79fd0078447d67002009
ocramz <zocca.marco@gmail.com>
2016-07-04 18:03:01 +0200

    specs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import Test.Hspec
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+
+
+-- | what are the invariants ?
+-- * setting an entity X to state Xs and observing state of entity X immediately afterwards should return Xs
